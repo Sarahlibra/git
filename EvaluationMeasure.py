a7b7996f9d1bed8ce275b5ab26d683b3dd4e7dc5
Sarahlibra <18280389724@163.com>
2016-03-16 21:08:59 +0800

    EvaluationMeasure.py二次提交

--- EvaluationMeasure.py
+++ EvaluationMeasure.py
@@ -19,9 +19,6 @@ def score(S, T):
 			r.remove('')
 		if u in T and len(r)>0:
 			t = T[u] #Test itemList
-			score1 = precisionAtK(r, t, 2)
-			score2 = recall(r,t)
-			score3 = userSuccess(r,t)
 			score+=20 * (precisionAtK(r, t, 2) + precisionAtK(r, t, 4) + recall(r, t) + userSuccess(r, t)) + 10 * (precisionAtK(r, t, 6) + precisionAtK(r, t, 20))
 	print ("prediction result:")
 	print (score)
@@ -30,20 +27,20 @@ def score(S, T):
 
 #precision within the first top k items: 
 def precisionAtK(recommendedItems, relevantItem, k):
-	topK = recommendedItems[0:k-1] #takes first k items from the list of reccommendedItems
-	return len(list(set(topK).intersection(set(relevantItem))))/k
+	topK = recommendedItems[0:k] #takes first k items from the list of reccommendedItems
+	return len(list(set(topK)&set(relevantItem)))/k
 
 
 #recall = fraction of relevant, retrieved items (30 items 
 #are allowed to be submitted at maximum per user): 
 def recall(recommendedItems, relevantItem):
 	if (len(relevantItem)> 0):
-		return	len(list(set(recommendedItems[0:29]).intersection(set(relevantItem))))/len(relevantItem)
+		return	len(list(set(recommendedItems[0:30])&set(relevantItem)))/len(relevantItem)
 	else: 
 		return 0.0
 #user success = was at least one relevant item recommended for a given user?
 def userSuccess(recommendedItems, relevantItem):
-	if len(list(set(recommendedItems[0:29]).intersection(set(relevantItem))))> 0:
+	if len(list(set(recommendedItems[0:30])&set(relevantItem))):
 		return 1.0
 	else:
 		return 0.0
